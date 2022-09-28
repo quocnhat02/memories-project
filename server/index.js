76eb49fb87648d6b21a0c15d175a147803cc2630
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 17:16:14 +0700

    mern 4

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
