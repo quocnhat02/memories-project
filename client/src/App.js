b86df99b1ae3c9f76160e8b69bb10753d2534179
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 19:42:45 +0700

    mern 21

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -8,6 +11,11 @@ import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
