ba9701fbecdb102ed2cbb88226d2ae5705fd6067
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 18:25:54 +0700

    mern 14

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
 import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return (
@@ -10,8 +12,25 @@ const App = () => {
         <Typography variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img src={memories} alt="memories" height="400" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
