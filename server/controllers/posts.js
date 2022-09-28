f07278f11fb92699dc5b3b5bc04c46845453323f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 18:05:05 +0700

    mern 9

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,11 @@
+import PostMessage from '../models/postMessage';
+
 const getPosts = (req, res) => {
   res.send('THIS WORKS!');
 };
 
-export { getPosts };
+const createPost = (req, res) => {
+  res.send('Post Creation');
+};
+
+export { getPosts, createPost };
