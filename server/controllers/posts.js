6648246672862420eab486b0f0d6d0272fae2a0c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 18:14:56 +0700

    mern 11

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,8 +10,17 @@ const getPosts = async (req, res) => {
   }
 };
 
-const createPost = (req, res) => {
-  res.send('Post Creation');
+const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+    return res.status(201).json(newPost);
+  } catch (error) {
+    return res.status(409).json({ message: error.message });
+  }
 };
 
 export { getPosts, createPost };
