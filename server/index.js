cd549d553f5d3f7af85eadb9cdea9d4d10a49370
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 17:32:27 +0700

    mern 5

--- server/index.js
+++ server/index.js
@@ -7,3 +7,20 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb://localhost:27017/memories-project';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
