63fee4b993866de7bece2578ae27f93a412b1342
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-28 18:20:14 +0700

    mern 12

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+
+import memories from './images/memories.png';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+    </Container>
+  );
 };
 
 export default App;
